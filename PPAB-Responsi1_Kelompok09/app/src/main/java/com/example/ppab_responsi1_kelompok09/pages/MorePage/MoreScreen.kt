e8d45fcee93c7d136012efc4c99a9a4bfbce3c66
Belajar Git <arilfadlahuda1234@student.uns.ac.id>
2025-05-24 09:47:02 +0700

    Nambahin Poppins sama custom shadow sesuai Figma

--- PPAB-Responsi1_Kelompok09/app/src/main/java/com/example/ppab_responsi1_kelompok09/pages/MorePage/MoreScreen.kt
+++ PPAB-Responsi1_Kelompok09/app/src/main/java/com/example/ppab_responsi1_kelompok09/pages/MorePage/MoreScreen.kt
@@ -1,7 +1,6 @@
 package com.example.ppab_responsi1_kelompok09.pages.MorePage
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,18 +25,24 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.ppab_responsi1_kelompok09.R
-import com.example.ppab_responsi1_kelompok09.component.HeaderGradient
-import com.example.ppab_responsi1_kelompok09.component.ProfileContainer
+import com.example.ppab_responsi1_kelompok09.common.component.HeaderGradient
+import com.example.ppab_responsi1_kelompok09.common.component.ProfileContainer
+import com.example.ppab_responsi1_kelompok09.common.style.dropShadow200
 import com.example.ppab_responsi1_kelompok09.ui.theme.Danger
+import com.example.ppab_responsi1_kelompok09.ui.theme.Dark
 import com.example.ppab_responsi1_kelompok09.ui.theme.Gray
+import com.example.ppab_responsi1_kelompok09.ui.theme.Poppins
 import com.example.ppab_responsi1_kelompok09.ui.theme.Primary
+import com.example.ppab_responsi1_kelompok09.ui.theme.White
 
+@Preview
 @Composable
 fun MoreScreen () {
     Box (
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+//            MaterialTheme.colorScheme.background
+            .background(White)
     ) {
         HeaderGradient()
         Column (
@@ -51,28 +56,52 @@ fun MoreScreen () {
                 "Biru",
                 true
             )
-            ManajemenItem(
-                true,
-                "",
-                R.drawable.pelanggan_fill
-            )
+            Box (
+                modifier = Modifier
+                    .dropShadow200(8.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                ManajemenItem(
+                    true,
+                    "",
+                    R.drawable.pelanggan_fill
+                )
+            }
             Text(
                 text = "Manajemen",
+                fontFamily = Poppins,
                 fontWeight = FontWeight.Medium,
                 fontSize = 14.sp,
                 color = MaterialTheme.colorScheme.onBackground
             )
             ManajemenItemContainer()
-            Logout()
+            Column (
+                modifier = Modifier
+                    .dropShadow200(8.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .fillMaxWidth()
+                    .height(60.dp)
+            ) {
+                Logout()
+            }
         }
     }
 }
 
 @Composable
 fun ManajemenItemContainer () {
-    ManajemenItem(false, "Profil Bisnis", R.drawable.home_fill)
-    ManajemenItem(false, "Kelola Saldo", R.drawable.home_fill)
-    ManajemenItem(false, "Lihat Laporan Keuangan", R.drawable.home_fill)
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .dropShadow200(8.dp)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+        ManajemenItem(false, "Profil Bisnis", R.drawable.home_fill)
+        Spacer(modifier = Modifier.height(0.5.dp).background(Dark.copy(0.2f)))
+        ManajemenItem(false, "Kelola Saldo", R.drawable.home_fill)
+        Spacer(modifier = Modifier.height(0.5.dp).background(Dark.copy(0.2f)))
+        ManajemenItem(false, "Lihat Laporan Keuangan", R.drawable.home_fill)
+    }
 }
 
 @Composable
@@ -84,8 +113,8 @@ fun ManajemenItem (
     Row (
         modifier = Modifier
             .fillMaxWidth()
-            .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.background)
+//            MaterialTheme.colorScheme.background
+            .background(White)
             .padding(horizontal = 16.dp)
             .height(60.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -106,7 +135,7 @@ fun ManajemenItem (
                     painter = painterResource(icon),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(32.dp)
+                        .size(24.dp)
                         .align(Alignment.Center),
                     tint = Primary
                 )
@@ -115,6 +144,7 @@ fun ManajemenItem (
                 Text(
                     text = "Akun saya",
                     color = Primary,
+                    fontFamily = Poppins,
                     fontWeight = FontWeight.Bold,
                     fontSize = 16.sp
                 )
@@ -122,6 +152,7 @@ fun ManajemenItem (
                 Text(
                     text = text,
                     color = MaterialTheme.colorScheme.onBackground,
+                    fontFamily = Poppins,
                     fontWeight = FontWeight.Normal,
                     fontSize = 14.sp
                 )
@@ -141,11 +172,11 @@ fun ManajemenItem (
 fun Logout () {
     Row (
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.background)
-            .padding(horizontal = 16.dp)
-            .height(60.dp),
+//          MaterialTheme.colorScheme.background
+            .background(White)
+            .padding(horizontal = 16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -163,13 +194,14 @@ fun Logout () {
                     painter = painterResource(R.drawable.previous),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(32.dp)
+                        .size(24.dp)
                         .align(Alignment.Center),
                     tint = Danger
                 )
             }
             Text(
                 text = "Logout",
+                fontFamily = Poppins,
                 color = Danger,
                 fontWeight = FontWeight.Normal,
                 fontSize = 14.sp
